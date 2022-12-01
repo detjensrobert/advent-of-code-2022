06bcdff73ac7be31cedb3f158f13539e46f77fc2
detjensrobert <github@detjens.dev>
2022-11-30 21:09:14 -0800

    day one ez

---
+++ day01/day1.rb
@@ -0,0 +1,15 @@
+#!/usr/bin/env ruby
+
+# Input is a list of elves, separated by double newlines
+# Each elf has a list of calorie counts of the food they are carrying.
+input = ARGF.read.split("\n\n").map { |elf| elf.split("\n").map &:to_i }
+
+# PART 1
+#
+# How many calories is the elf with the maximum number of calories carrying?
+puts "Part 1: max calories: #{input.map(&:sum).max}"
+
+# PART 2
+#
+# What is the sum of the top three elves calories?
+puts "Part 2: Sum of top three: #{input.map(&:sum).sort[-3..].sum}"
